4babf0ac12e567e1265b030b77710bed488f2ab4
wilsoncar <wilsoncardona73@gmail.com>
2022-03-28 16:32:33 -0600

    Implementación propia del algoritmo de luhn

--- routes/luhn.js
+++ routes/luhn.js
@@ -1,97 +1,60 @@
 const express = require('express');
 
+
 function luhnApi(app) {
+
     const router = express.Router();
+    
     app.use("/luhn", router);
-
-    router.get("/", async function(req, res, next){
-        try {
-          res.status(200).json({
-              isValid: isValidNumberCreditCard()
-          });
-        } catch (error) {
-            next(err);
-        }
-    });
-
-    router.post("/", async function(req, res, next){
-        const { body: number } = req;
-        console.log('req', number);
-        try {
-            res.status(200).json({
-                isValid: await isValidNumberCreditCard(number)
-            });
-        } catch (error) {
-            next(err);
-        }
+    
+    router.post("/", (req, res)=>{
+        let {credit} = req.body;
+        console.log("Valor body:",credit);
+        res.status(200).json({
+            isValid: isValidNumberCreditCard(credit)
+        })
+    
     });
 
     function split_numbers(n) {
-        console.log('split_numbers', n);
-        return new Promise((resolve) => {
-            if (n.number) {
-                resolve((n.number + '').split('').map((i) => { return Number(i); }));
-            } else {
-                resolve((n + '').split('').map((i) => { return Number(i); }));
-            }
-        });
+        return (n + '').split('').map((i) => { return Number(i); });
     }
 
-    async function luhn(n) {
-        console.log('luhn', n);
-        const number_splitted = await split_numbers(n);
-        console.log('number_splitted', number_splitted);
-        const number_reversed = number_splitted.reverse();
-
-        let result;
-        let results = [];
-
-        for (let i=0; i<number_reversed.length; i++) {
-            const even_number = i%2;
-            if (even_number == 0) {
-                result = number_reversed[i] * 1;
-                results.push(result);
-            } else {
-                result = number_reversed[i] * 2;
-                if (result > 9) {
-                    result = await isGraterThanNine(result);
+    function luhn(credit) {
+        const numcredit = split_numbers(credit);
+        let n =0;
+        let sum=0;
+        numlength = numcredit.length;
+        for (let i=numlength - 1; i >=0; i--){
+            if(i%2==0){
+                n=numcredit[i];
+            }
+            else {
+                n=numcredit[i]*2;
+                if(n>9){
+                    n=(n-10) + 1;
                 }
-                results.push(result);
             }
-
+            sum = sum+n;
+            console.log("valor n: ", n++)
         }
-        return results;
+        console.log("suma: ",sum);
+        return sum;
     }
 
-    async function isGraterThanNine(result) {
-        const value = await split_numbers(result);
-        console.log('value', value);
-        let plus = 0;
-        for (let i=0; i<value.length; i++) {
-            plus = plus + parseInt(value[i].toString(),10);
-        }
-        return plus;
-    }
-
-    async function isValidNumberCreditCard(n) {
-        console.log('isValidNumberCreditCard', n);
-        const results = await luhn(n);
+    function isValidNumberCreditCard(credit) {
+        const result = luhn(credit);
         let isValid = false;
-        let plus = 0;
-        results.forEach(element => {
-            plus = plus + element;
-        });
-        base = plus%10;
-        if (base == 0) {
-            isValid = true;
-        } else {
-            isValid = false;
+        if (result%10==0){
+            isValid=true;
+            console.log("\nNúmero válido\n");
+        }
+        else{
+            isValid=false;
+            console.log("\nNúmero no válido\n");
         }
         return isValid;
     }
-
 }
-
-
 
 module.exports = luhnApi;
